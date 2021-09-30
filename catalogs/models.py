5d1cad6afdecdd7e37bf491632a460c2ec846e71
Carl <cruzthecarl10@gmail.com>
2021-09-30 13:33:53 +0800

    trial models

--- catalogs/models.py
+++ catalogs/models.py
@@ -1,4 +1,24 @@
 from django.db import models
 
-class Book(models.Model):
-    isbn = models.CharField(max_length=11)
+
+# Book Model
+class TitleProper(models.Model):
+    title_proper = models.CharField(max_length=255)
+    responsibility = models.CharField(max_length=255)
+    preferred_title = models.CharField(max_length=255)
+    parallel_title = models.CharField(max_length=255)
+    main_creator = models.CharField(max_length=255)
+    added_entry_creator = models.CharField(max_length=255)
+    contributors = models.CharField(max_length=255)
+    added_entry_corporate = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.title_proper
+
+class Publication(models.Model):
+    place_of_publication = models.CharField(max_length=255)
+    publisher = models.CharField(max_length=255)
+    date_of_publication = models.DateField()
+
+    def __str__(self):
+        return self.publisher
