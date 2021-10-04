bf8951a65ae61f4e382a9bf45ef14207ee5043e5
unknown <carl.cruz@shap.edu.ph>
2021-10-04 09:04:05 +0800

    added unit test for book creation

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,12 +1,11 @@
-from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from django.db import transaction
 from django.forms.models import ModelForm
 
 from accounts.models import Employee, MyUser
 
-class EmployeeForm(ModelForm):
 
+class EmployeeForm(ModelForm):  # Employee form for registration
+    """ Added an extra field for department for employee registration"""
     class Meta:
         model = Employee
         fields = ('department',)
--- catalogs/tests.py
+++ catalogs/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from catalogs.models import Book
 
-# Create your tests here.
+
+class BookTestcase(TestCase):
+    def setUp(self):
+        Book.objects.create(title_proper="New Book 1",
+                            place_of_publication="New Book 1 Publication")
+
+    def test_book_has_place_of_publication(self):
+        """ Book must have a place of publication"""
+        new_book = Book.objects.get(title_proper="New Book 1")
+        self.assertEqual(new_book.place_of_publication,
+                         "New Book 1 Publication")
