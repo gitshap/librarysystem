2af20d6cbc38dd9e9e6c3f1fe225a39f495ecf93
unknown <carl.cruz@shap.edu.ph>
2021-10-04 15:12:54 +0800

    minor UI tweaks

--- catalogs/forms.py
+++ catalogs/forms.py
@@ -1,10 +1,22 @@
 from django.db.models.base import Model
 from django.forms import ModelForm
+from crispy_forms.helper import FormHelper
 
 from catalogs.models import Book
 
 
 class TitleProperForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(TitleProperForm, self).__init__(*args, **kwargs)
+        self.fields['title_proper'].label = ""
+        self.fields['responsibility'].label = ""
+        self.fields['preferred_title'].label = ""
+        self.fields['parallel_title'].label = ""
+        self.fields['main_creator'].label = ""
+        self.fields['added_entry_creator'].label = ""
+        self.fields['contributors'].label = ""
+        self.fields['added_entry_corporate'].label = ""
+
     class Meta:
         model = Book
         fields = ['title_proper', 'responsibility', 'preferred_title', 'parallel_title',
@@ -12,6 +24,11 @@ class TitleProperForm(ModelForm):
 
 
 class PublicationForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(PublicationForm, self).__init__(*args, **kwargs)
+        self.fields['place_of_publication'].label = ""
+        self.fields['publisher'].label = ""
+
     class Meta:
         model = Book
         fields = ['place_of_publication', 'publisher']
