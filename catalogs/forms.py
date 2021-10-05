4576fc2fa34f3193cc6ea2e35ec571fbb5162c99
Carl <cruzthecarl10@gmail.com>
2021-10-05 08:07:35 +0800

    modified book forms

--- catalogs/forms.py
+++ catalogs/forms.py
@@ -5,30 +5,20 @@ from crispy_forms.helper import FormHelper
 from catalogs.models import Book
 
 
-class TitleProperForm(ModelForm):
+class BookForm(ModelForm):
     def __init__(self, *args, **kwargs):
-        super(TitleProperForm, self).__init__(*args, **kwargs)
-        self.fields['title_proper'].label = ""
-        self.fields['responsibility'].label = ""
-        self.fields['preferred_title'].label = ""
-        self.fields['parallel_title'].label = ""
-        self.fields['main_creator'].label = ""
-        self.fields['added_entry_creator'].label = ""
-        self.fields['contributors'].label = ""
-        self.fields['added_entry_corporate'].label = ""
+        super(BookForm, self).__init__(*args, **kwargs)
+        self.fields["title_proper"].label = ""
+        self.fields["responsibility"].label = ""
+        self.fields["preferred_title"].label = ""
+        self.fields["parallel_title"].label = ""
+        self.fields["main_creator"].label = ""
+        self.fields["added_entry_creator"].label = ""
+        self.fields["contributors"].label = ""
+        self.fields["added_entry_corporate"].label = ""
+        self.fields["place_of_publication"].label = ""
+        self.fields["publisher"].label = ""
 
     class Meta:
         model = Book
-        fields = ['title_proper', 'responsibility', 'preferred_title', 'parallel_title',
-                  'main_creator', 'added_entry_creator', 'contributors', 'added_entry_corporate']
-
-
-class PublicationForm(ModelForm):
-    def __init__(self, *args, **kwargs):
-        super(PublicationForm, self).__init__(*args, **kwargs)
-        self.fields['place_of_publication'].label = ""
-        self.fields['publisher'].label = ""
-
-    class Meta:
-        model = Book
-        fields = ['place_of_publication', 'publisher']
+        fields = "__all__"
