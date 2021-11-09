12a7fa158584e0f6b9e468040d96f67895cc1a90
Carl <cruzthecarl10@gmail.com>
2021-11-09 16:31:15 +0800

    completed book crud

--- catalogs/urls.py
+++ catalogs/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 
-from catalogs.views import create_book
+from catalogs.views import create_book, view_book, update_book, delete_book
 
 app_name = 'catalogs'
 urlpatterns = [
     path('book/create-book', create_book, name='create-book'),
+    path('book/update_book/<str:title_proper>',
+         update_book, name='update_book'),
+    path('book/view_book/<str:title_proper>', view_book, name='view_book'),
+    path('book/delete_book/<str:title_proper>', delete_book, name='delete_book')
+
 ]
--- catalogs/views.py
+++ catalogs/views.py
@@ -2,6 +2,9 @@ from django.http.response import HttpResponse
 from django.shortcuts import redirect, render
 from catalogs.models import Book
 from catalogs.forms import BookForm
+from accounts.views import home_view
+from django.contrib import messages
+from django.http import HttpResponseRedirect
 
 
 def create_book(request):
@@ -18,6 +21,49 @@ def create_book(request):
         book_form = BookForm()  # otherwise create a new blank form
 
     context = {
+
         "book_form": book_form,
     }
     return render(request, "partials/create_book.html", context=context)
+
+
+# view specific book
+def view_book(request, title_proper):
+    book = Book.objects.get(title_proper=title_proper)
+
+    template_name = 'view_book.html'
+
+    context = {
+        'book': book
+    }
+
+    return render(request, template_name, context=context)
+
+
+def update_book(request, title_proper):
+    book = Book.objects.get(title_proper=title_proper)
+    form = BookForm()
+    template_name = 'view_book.html'
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid:
+            form.save()
+            # add a message saying success
+            messages.success(request, "Added a new book")
+            return redirect(home_view)
+        else:
+            messages.warning(request, "Please see errors")
+    context = {
+        'book': book,
+        'form': form,
+    }
+    return render(request, template_name, context=context)
+
+
+def delete_book(request, title_proper):
+    book = Book.objects.get(title_proper=title_proper)
+    book.delete()
+    messages.success(request, "Book deleted")
+    return redirect(home_view)
+
+# TODO: DRY ( Mix everything in into one view )
